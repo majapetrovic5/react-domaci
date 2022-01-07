b15018007e25fdabdca2db4577df7debd4844526
majapetrovic5 <76411291+majapetrovic5@users.noreply.github.com>
2022-01-07 12:51:18 +0100

    Napravljen MainSection

---
+++ src/components/MainSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import './MainSection.css';
+
+
+function MainSection() {
+  return (
+    <div className='main-container'>
+      <h1>FIND THE BEST DOCTOR </h1>
+        <h1>FOR YOU</h1>
+      <p>Make an appointment and contribute to your health</p>    
+      </div>
+  )
+}
+
+export default MainSection;
