984c6b339fab59f979eacb44d62720f3c87baf12
majapetrovic5 <76411291+majapetrovic5@users.noreply.github.com>
2022-01-07 23:17:58 +0100

    Azurirana komponenta Client

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Quote from './Quote';
+import {Button} from './Button';
 
 
-function Client({text,name, picture}) {
+
+function Client({text,name,picture,spec, type}) {
   return (
     <>
       <li className='client'>
           <div className='client_wrap'>
-          <figure className='client__pic-wrap' data-category={'”'}>
+          <figure className='client__pic-wrap' data-category={type=='clients'? '"': spec}>
     
             <img
               className='client__img'
@@ -18,6 +19,27 @@ function Client({text,name, picture}) {
            <div className='client__info'>
             <p className='client__text'>{text}</p>
             <h5 className='client__name'>{name}</h5>
+            {type==='doctors'? 
+            <div>
+              <ul>
+             <li>
+               <div className='star'>
+             <i class="fas fa-star"></i>
+             <p>{Math.floor((Math.random() * 5) + 1)}</p>
+             </div>
+             
+             </li>
+            <li> <Button
+          className='btns'
+          buttonStyle='btn--test'
+          buttonSize='btn--medium' link='/doctors'
+        >
+          MAKE AN APPOINTMENT
+        </Button></li> </ul>  
+ 
+            </div>
+          
+            : <></>}
            
             </div>
           </div>
--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import './Clients.css';
 import Client from './Client'; 
-import Img1 from '../assets/images/img-client1.jpg';
-import Img2 from '../assets/images/img-clients2.jpg';
-import Img3 from '../assets/images/img-client3.jpg';
 
-function Clients() {
+
+function Clients({title, array1, array2, type}) {
   return (
     <div className='clients'>
-      <h1>WHAT OUR CLIENTS SAYS?</h1>
+      <h1>{title}</h1>
       <div className='clients__container'>
         <div className='clients__wrapper'>
           <ul className='clients__items'>
-            <Client
-              text='It is a long established fact that a reader will be distracted by the 
-              readable content of a page when looking at its layout.'
-              name='Maria Reyes'
-              picture={Img1}
-            />
-            <Client
-              text='It is a long established fact that a reader will be 
-              distracted by the readable content of a page when looking at its layout.'
-              name='Robert Gilbert'
-              picture={Img2}
-            />
-            <Client
-              text='It is a long established fact that a reader will be 
-              distracted by the readable content of a page when looking at its layout.'
-              name='Chuck Cooper'
-              picture={Img3} />
-             
-          </ul>
+        
+{
+  array1.map(item => (<Client name={item.name} text={item.text} picture={item.picture}
+    spec={item.spec }type={type}>  </Client>)) }
+</ul>
+{
+ array2 != null ? <ul className='clients__items'>
+ {
+   array2.map(item => (<Client name={item.name} text={item.text} picture={item.picture}
+    spec={item.spec}type={type}>  </Client>)) }
+</ul> : <></>
+}
+          
         </div>
       </div>
     </div>
